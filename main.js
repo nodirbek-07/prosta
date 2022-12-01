75859623c5c071a8c8d1c11284631915f3f98129
Aziz <nodirbekma07@gmail.com>
2022-12-01 18:10:07 +0500

    finished site

---
+++ main.js
@@ -0,0 +1,49 @@
+// let random = document.querySelector('.random');
+
+// random.addEventListener('click' , function(){
+//     body.style.background = myrandom(),
+//     body.style.transition = '0.5s'
+// })
+
+// function myrandom(){
+//     let r = Math.floor(Math.random() * 255 + 1)
+//     let g = Math.floor(Math.random() * 255 + 1)
+//     let b = Math.floor(Math.random() * 255 + 1)
+
+
+//     let rgb = `rgb(${r} ,${g} ,${b})`;
+
+//     return rgb
+    
+// }
+
+// let str = prompt('Xush kelipsiz - Добро Пожаловать - Welcom');
+
+// if (str == 'Salom') {
+//     alert('Assalomalekum');
+// }   else if (str == 'Привет') {
+//     alert('Здраствуйте');
+// } else if (str == 'Hello') {
+//     alert('Hay');
+// } else {
+//     alert('Error...!')
+// }
+
+// var elem = document.querySelector('.writer');
+
+// let text = elem.innerHTML;
+
+// elem.innerHTML = ' '
+
+// let counter = 0;
+// function writter() {
+//     elem.innerHTML += text.charAt(counter);
+
+//     counter ++;
+
+//     if(text.length == counter ) {
+//         clearInterval(timer);
+//     }
+// }
+
+// let timer = setInterval(writer , 200);
